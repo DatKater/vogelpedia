1721595d49405ff35a2feab28fb14b6a83df9a40
similehni <simon.lehnert@studmail.kzo.ch>
2016-03-29 09:06:30 +0200

    + Probe-Formular

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Vogelpedia - Ein Vogelprojekt!</title>
-        
         <link href="/vogelpedia/css/styles.css" rel="stylesheet">
     </head>
     <body>
@@ -22,6 +21,19 @@ and open the template in the editor.
         </nav>
         </header>
         <h1>Vogelpedia</h1>
+        <form action="" method="">
+            <label for="Vogelname">Vogelname: </label><input type="text" name="Vogelname" value="" size="30" maxlength="50"> <br>
+            <label for="familie">Vogelfamilie: </label><select name="familie"><option value="meisen">Meisen</option><option value="finken">Finken</option></select> <br>
+            <label for="gebirge">Lebensraum: </label><select name="lebenraum"><option value="gebirge">Gebirge</option><option value="feuchtgebiete">Feuchtgebiete</option><option value="wald">Wald</option></select> <br>
+            <label for="color">Farbe: </label><select name="color"><option value="gelb">Gelb</option><option value="gruen">Grün</option><option name="schwarz">Schwarz</option></select> <br>
+            <label for="nahrung">Nahrung: </label><select name="nahrung"><option value="Kaefer">Käfer</option><option value="Wuermer">Würmer</option><option value="Bienen">Bienen</option></select> <br>
+            <label for="Brutort">Brutort: </label><select name="Brutort"><option value="Boden">Boden</option><option value="Baumhoehle">Baumhöhle</option><option value="Busch">Busch</option></select><br>
+            <input type="submit"><input type="reset">
+            
+        
+        
+        </form>
+        
         <?php
         
         ?>
