a63788fe1f4aa42187eb63d73e03eda462146309
DatKater <imperator.katz@gmail.com>
2016-04-16 20:19:51 +0200

    Update
    M2M Funktionalität

--- add.php
+++ add.php
@@ -9,30 +9,72 @@
                 require_once('settings/object.php');
                 require_once('functions.php');
                 require_once('renderer/object.php');
+                
                 $dbHandler = get_handler();
                 $renderer = new TemplateRenderer();
                 
-                function get_select_fk(){
-                    $select = "<p><label for=''>%s</label></p>";
+                if(isset($_POST['debug'])) {
+                    $debug = $_POST['debug'];
+                    unset($_POST['debug']);
+                } else {
+                    $debug = NULL;
                 }
                 
+                $m2m = array(
+                    'bird_has_color' => array(
+                        'fk_id' => 'idcolor',
+                        'id' => 'idbird',
+                    ),
+                    'bird_has_food' => array(
+                        'fk_id' => 'idfood',
+                        'id' => 'idbird',
+                    ),
+                    'bird_has_habitat' => array(
+                        'fk_id' => 'idhabitat',
+                        'id' => 'idbird',
+                    ),
+                );
+                
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                    $bound_variables = prepare_bound_variables($_POST);
-                    $bound = join(', ', array_keys($bound_variables));
-                    $columns = join(', ', array_keys($_POST));
+                    $split_m2m = split_m2m($m2m, $_POST);
+                    $no_m2m = $split_m2m[0];
+                    $m2m_rel = $split_m2m[1];
                     
-                    $query = 'INSERT INTO bird (%s) VALUES (%s);';
-                    $query = sprintf($query, $columns, $bound);
-
-                    $stmt = $dbHandler->prepare($query);
-                    $stmt->execute($bound_variables);
-                    $stmt->closeCursor();
+                    $bound_variables = prepare_bound_variables($no_m2m);
+                    $bound = join(', ', array_keys($bound_variables));
+                    $columns = join(', ', array_keys($no_m2m));
+                    
+                    $query = sprintf('INSERT INTO bird (%s) VALUES (%s);', $columns, $bound);
+                    
+                    if(!$debug){
+                        $stmt = $dbHandler->prepare($query);
+                        $stmt->execute($bound_variables);
+                        $stmt->closeCursor();
+                        
+                        $id = $dbHandler->lastInsertId();
+                        echo '<br> ID',$id,'</br>';
+                        handle_m2m($m2m_rel, $id, $dbHandler);
+                        
+                    } else {
+                        print_r($query);
+                        echo '<pre>';
+                        print_r($no_m2m);
+                        print_r($m2m_rel);
+                        print_r($_POST);
+                        echo '</pre>';
+                        
+                    }
                 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
                     $family_keys = get_values_from_db($dbHandler, 'family');
                     $breeding_place_keys = get_values_from_db($dbHandler, 'breeding_place');
+                    
+                    $color_keys = get_values_from_db($dbHandler, 'color');
+                    $food_keys = get_values_from_db($dbHandler, 'food');
+                    $habitat_keys = get_values_from_db($dbHandler, 'habitat');
                 }
                 
             ?>
+            
             <p><label for='name'>Name:</label><input type='text' id='name' name='name'></p>
             <p><label for='name_latin'>Name in Latein:</label><input type='text' id='name_latin' name='name_latin'></p>
             <p><label for='min_livestock'>Bestand:</label><input type='number' id='min_livestock' name='min_livestock'> bis <input type='number' id='max_livestock' name='max_livestock'></p>
@@ -44,28 +86,37 @@
             <p><label for='red_list'>Rote Liste:</label><input type='checkbox' id='red_list' name='red_list'></p>
             
             <?php
-//                echo $renderer->render('widgets/foreign_key.html', array('id' => 'family',
-//                    'name' => 'family_idfamily', 'verbose_name' => 'Familie',
-//                    'array' => $family_keys, 'fk_id' => 'idbird', 'fk_name' => 'name'));
-//                print_r($family_keys);
+                echo $renderer->render('widgets/foreign_key.html', array('id' => 'family',
+                    'name' => 'family_idfamily', 'verbose_name' => 'Familie',
+                    'array' => $family_keys, 'fk_id' => 'idfamily', 'fk_name' => 'name',
+                    'multiple' => ''));
+                
+                echo $renderer->render('widgets/foreign_key.html', array('id' => 'breeding_place',
+                    'name' => 'breeding_place_idbreeding_place', 'verbose_name' => 'Brutort',
+                    'array' => $breeding_place_keys, 'fk_id' => 'idbreeding_place', 'fk_name' => 'name',
+                    'multiple' => ''));
+                
+                echo $renderer->render('widgets/foreign_key.html', array('id' => 'bird_has_color',
+                    'name' => 'bird_has_color[]', 'verbose_name' => 'Farbe',
+                    'array' => $color_keys, 'fk_id' => 'idcolor', 'fk_name' => 'color_name',
+                    'multiple' => 'multiple'));
+                
+                echo $renderer->render('widgets/foreign_key.html', array('id' => 'bird_has_food',
+                    'name' => 'bird_has_food[]', 'verbose_name' => 'Nahrung',
+                    'array' => $food_keys, 'fk_id' => 'idfood', 'fk_name' => 'name',
+                    'multiple' => 'multiple'));
+                
+                echo $renderer->render('widgets/foreign_key.html', array('id' => 'bird_has_habitat',
+                    'name' => 'bird_has_habitat[]', 'verbose_name' => 'Habitat',
+                    'array' => $habitat_keys, 'fk_id' => 'idhabitat', 'fk_name' => 'name',
+                    'multiple' => 'multiple'));
+                
             ?>
             
-            <p><label for='family'>Familie:</label><select id='family' type="number" name='family_idfamily'>
-            <?php
-                echo print_select_options($family_keys, 'idbird', 'name');
-            ?>
-            </select></p>
-            
-            <p><label for='breeding_place'>Brutort:</label><select id='breeding_place' type="number" name='breeding_place_idbreeding_place'>
-            <?php
-                echo print_select_options($breeding_place_keys, 'idbreeding_place', 'name');
-            ?>
-            </select></p>
+            <p><label for='description'>Beschreibung:</label><textarea id='description' name='description'></textarea></p>
+            <p><label for='debug'>Debug:</label><input type='checkbox' id='debug' name='debug'></p>
             
             <p><input type='submit' value='Absenden'><input type='reset' value='Zurücksetzen'></p>
-            <?php
-                echo $renderer->render('widgets/foreign_key.html', array('array' => $breeding_place_keys, 'id' => 'idbreeding_place', 'name' => 'name'));
-            ?>
         </form>
     </fieldset>
 <?php include 'parts/footer.php' ?>
--- functions.php
+++ functions.php
@@ -28,7 +28,6 @@ function get_values_from_db($handler, $table){
 }
 
 function print_select_options($array, $id, $name) {
-    print_r($key);
     $options = '';
     foreach($array as $key){
         $options .= sprintf('<option value="%s">%s</option>', $key[$id], $key[$name]);
@@ -36,4 +35,34 @@ function print_select_options($array, $id, $name) {
     return $options;
 }
 
+function split_m2m($m2m, $post){
+    foreach($m2m as $key => $val){
+        if(isset($post[$key])){
+            $m2m[$key]['value'] = $post[$key];
+        }
+        unset($post[$key]);
+    }
+    return array($post, $m2m);
+}
+
+function handle_m2m($m2m, $id, $handler) {
+    foreach($m2m as $table => $array) {
+        $fk_id_name = $array['fk_id'];
+        $id_name = $array['id'];
+        $values = $array['value'];
+        
+        $query = sprintf("INSERT INTO %s (%s, %s) VALUES (:fk_id, '%s')", $table, $fk_id_name, $id_name, $id);
+        $stmt = $handler->prepare($query);
+        
+        echo $query;
+        print_r($values);
+        
+        foreach($values as $val) {
+            $stmt->bindParam(':fk_id', $val);
+            $stmt->execute();
+        }
+        $stmt->closeCursor();
+        
+    }
+}
 ?>
