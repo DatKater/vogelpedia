d873269057cd63e05b9a0b12d8c8603b061e63eb
similehni <simon.lehnert@studmail.kzo.ch>
2016-05-09 17:34:18 +0200

    Add files via upload

--- parts/header.php
+++ parts/header.php
@@ -1,15 +1,15 @@
-<header>
+<div id="header">
 <nav>
     <ul class="nav">
         <li><a href="/vogelpedia/index.php">Home</a></li>
         <li><a href="/vogelpedia/add.php">Vogel hinzufügen</a></li>
-        <li>
+        <li id="search">
             <form action="/vogelpedia/search.php" method="get" id="header_form">
-                <input type="text" name="name" placeholder="Vogel finden">
-                <input type="submit" value="Suche">
+                <input id="searchbar" type="text" name="name" placeholder="Vogel finden">
+                <input id="submit" type="submit" value="Suche">
             </form>
         </li>
-        <li><a href="/vogelpedia/search.php">Erweitert</a></li>
+        <li><a href="/vogelpedia/search.php">Erweiterte Suche</a></li>
     </ul>
 </nav>
-</header>
+</div>
