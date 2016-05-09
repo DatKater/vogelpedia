c6c834152341a9d967e7051cbb26857092f987d1
similehni <simon.lehnert@studmail.kzo.ch>
2016-05-09 22:47:36 +0200

    Design angepasst

--- index.php
+++ index.php
@@ -1,35 +1,40 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
 <?php include 'parts/head.php' ?>
-    <body>
-        <?php include 'parts/header.php' ?>
+<body>
+<?php include 'parts/header.php' ?>
+        <div id="main"><div id="inhalt">
+        <br>
+        <br>
+        <br>
         <h1>Vogelpedia</h1>
-        
+
         <?php
-        
+
         require_once 'settings/object.php';
         require_once 'renderer/object.php';
         require_once '/db_access/models.php';
         require_once 'functions.php';
-        
+
         $query = 'SELECT * FROM bird;';
         $dbHandler = get_handler();
-        $objects = get_single_value_from_db($dbHandler, 'bird', 'idbird, name');
+        $objects = get_single_value_from_db($dbHandler, 'bird', 'idbird, name, name_latin');
+        $i = count($objects);
+        echo "Momentan sind ".$i." V&ouml;gel in der Datenbank eingetragen.";
         ?>
-        
+        <br>
+        <br>
+        <div id="result">
         <ul>
         <?php
         foreach($objects as $object) {
-            echo '<li><a href="/vogelpedia/detail.php?pk='.$object['idbird'].'">'.$object['name'].'</a></li>';
+            echo '<li><a href="/vogelpedia/detail.php?pk='.$object['idbird'].'">'.$object["name"].',  <i>'.$object["name_latin"].'</i></a></li>';
         }
         ?>
         </ul>
-        
+        </div>
+        <br>
+        <br>
+        </div></div>
         <?php include 'parts/footer.php' ?>
     </body>
 </html>
